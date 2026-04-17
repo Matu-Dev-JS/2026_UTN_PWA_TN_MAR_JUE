cf3034692488aafe37c6ad5b2fb29a1b1fccf6e7
matu.dev <mati.a.gimenez2002@gmail.com>
2026-04-16 22:03:24 -0300

    Clase 10

--- Clase-10/main.js
+++ Clase-10/main.js
@@ -34,8 +34,60 @@ Crear una clase llamada ProductsManager
 con 2 metodos:
 - add -> Permite añadir un producto
 - deleteById -> Permite eliminar un producto por id
+- get -> Permite obtener la lista de productos (Lo lee directo del archivo (Solo una fuente de verdad))
 
 Aclaraciones:
 - La clase tendra manejo de IDs autogenerados (Recomendacion: Tener algun archivo donde guardes el conteo y empiece en 0, ej product-id-counter.txt)
 - La lista de usuarios persiste en memoria, en el archivo products.json
+- Recomiendo usar funciones o metodos internos: get(), save(), getNewId()
+
 */
+
+class UsersManager {
+    static USER_COUNTER_ID_FILE_NAME = 'users-id-counter.txt'
+    static USER_FILE = 'users.json'
+
+    get(){
+        const products_JSON = Filesystem.readFileSync(UsersManager.USER_FILE, 'utf-8')
+        return JSON.parse(products_JSON)
+    }
+
+    save (users){
+        Filesystem.writeFileSync(UsersManager.USER_FILE, JSON.stringify(users))
+    }
+
+    genNewId(){
+        /* 
+        Paso 1: Lee el archivo donde se guarda el id (users-id-counter) y lo pasamos a numero
+        Paso 2: Guardo el valor como id
+        Paso 3: Sobreescribo el archivo con id + 1 
+        */
+        let new_id = Filesystem.readFileSync(UsersManager.USER_COUNTER_ID_FILE_NAME, 'utf-8')
+        new_id = Number(new_id) + 1
+        Filesystem.writeFileSync(UsersManager.USER_COUNTER_ID_FILE_NAME, new_id, 'utf-8')
+        return new_id
+    }
+    add(name){
+        const users = this.get()
+        const new_id = this.genNewId()
+        const new_user = {
+            id: new_id,
+            name: name
+        }
+        users.push(new_user)
+        this.save(users)
+    }
+
+    deleteById(id){
+        /* 
+        -paso 1 llamar a la lista del archivo y pasar a objeto
+        -paso 2 eliminar producto
+        -paso 3 sobreescribir
+        */
+        const users = this.get()
+        const users_updated = users.filter(user => Number(id) !== Number(id))
+        this.save(users_updated)
+    }
+
+
+}
