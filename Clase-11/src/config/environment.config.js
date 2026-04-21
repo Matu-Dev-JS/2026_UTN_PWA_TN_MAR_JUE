21e3231f9c4eefebcdac2dc1edde8bd680ec1456
matu.dev <mati.a.gimenez2002@gmail.com>
2026-04-21 20:57:00 -0300

    coneccion mongodb

--- Clase-11/src/config/environment.config.js
+++ Clase-11/src/config/environment.config.js
@@ -5,7 +5,8 @@ dotenv.config()
 
 //Guardar en mismo lugar las variables de entorno
 const ENVIRONMENT = {
-    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING
+    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
+    MONGO_DB_NAME: process.env.MONGO_DB_NAME
 }
 
 export default ENVIRONMENT
--- Clase-11/src/main.js
+++ Clase-11/src/main.js
@@ -1,4 +1,7 @@
-import ENVIRONMENT from "./config/environment.js";
+import ENVIRONMENT from "./config/environment.config.js";
+import connectMongoDB from "./config/mongodb.config.js";
 import { sumar } from "./math.js";
 
 console.log(ENVIRONMENT.MONGO_DB_CONNECTION_STRING)
+
+connectMongoDB()
