a709030d699aad059a68186001be692d26b1556c
matu.dev <mati.a.gimenez2002@gmail.com>
2026-04-21 21:22:39 -0300

    Conexion con mongodb

--- Clase-11/src/main.js
+++ Clase-11/src/main.js
@@ -2,6 +2,7 @@ import ENVIRONMENT from "./config/environment.config.js";
 import connectMongoDB from "./config/mongodb.config.js";
 import { sumar } from "./math.js";
 
+
 console.log(ENVIRONMENT.MONGO_DB_CONNECTION_STRING)
 
 connectMongoDB()
