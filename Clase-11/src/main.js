6c9078785bfa38dab4306f580175d94f333622e8
matu.dev <mati.a.gimenez2002@gmail.com>
2026-04-21 21:43:38 -0300

    Clase 11

--- Clase-11/src/main.js
+++ Clase-11/src/main.js
@@ -1,8 +1,10 @@
 import ENVIRONMENT from "./config/environment.config.js";
 import connectMongoDB from "./config/mongodb.config.js";
 import { sumar } from "./math.js";
+import User from "./models/user.model.js";
 
 
 console.log(ENVIRONMENT.MONGO_DB_CONNECTION_STRING)
 
 connectMongoDB()
+
