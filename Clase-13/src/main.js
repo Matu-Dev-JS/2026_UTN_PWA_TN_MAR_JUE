e8ca4aaaaaa695fe74608f275bac2d52632a81f7
matu.dev <mati.a.gimenez2002@gmail.com>
2026-04-28 20:21:30 -0300

    Clase 13

---
+++ Clase-13/src/main.js
@@ -0,0 +1,28 @@
+import ENVIRONMENT from "./config/environment.config.js";
+import connectMongoDB from "./config/mongodb.config.js";
+import { sumar } from "./math.js";
+import User from "./models/user.model.js";
+
+/* SOLO EN LOCAL Y SI TENER PROBLEMAS DE DNS PARA CONECTARTE A MONGODB */
+import dns from 'dns';
+import userRepository from "./repositories/user.repository.js";
+
+if(ENVIRONMENT.MODE === 'development'){
+    dns.setServers(['8.8.8.8', '8.8.4.4']);
+}
+
+connectMongoDB()
+//userRepository.create('pepe', 'pepe@gmail.com', 'pepe123')
+
+/* userRepository.updateById(
+    '69eaa482681999efac4eb3bd', {
+        nombre: 'juan'
+    }
+) */
+
+/* userRepository.getById('69eaa482681999efac4eb3bd')
+.then(
+    (resultado) => {
+        console.log(resultado)
+    }
+) */
