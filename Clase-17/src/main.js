7119c12bacc43feb7b6e7015a425461fe8e041a5
matu.dev <mati.a.gimenez2002@gmail.com>
2026-05-12 19:17:25 -0300

    Clase 17 API Express con DB

---
+++ Clase-17/src/main.js
@@ -0,0 +1,40 @@
+import ENVIRONMENT from "./config/environment.config.js";
+import connectMongoDB from "./config/mongodb.config.js";
+import { sumar } from "./math.js";
+import User from "./models/user.model.js";
+
+/* SOLO EN LOCAL Y SI TENER PROBLEMAS DE DNS PARA CONECTARTE A MONGODB */
+import dns from 'dns';
+import userRepository from "./repositories/user.repository.js";
+import workspaceMemberRepository from "./repositories/workspaceMember.repository.js";
+import workspaceRepository from "./repositories/workspace.repository.js";
+import { MEMBER_WORKSPACE_ROLES } from "./constants/memberRoles.constant.js";
+
+if(ENVIRONMENT.MODE === 'development'){
+    dns.setServers(['8.8.8.8', '8.8.4.4']);
+}
+
+connectMongoDB()
+
+/* 
+Crear una API de express
+Route:
+    /api/auth
+        POST /register
+            body: {name, email, password}
+            Crear un usuario en la DB
+            Validar que el usuario tenga nombre mayor a 2 caracteres
+            Validar email
+            Validar password con almenos 6 caracteres 
+
+
+
+Mas Adelante...
+        POST /login
+        
+*/
+
+
+.catch(error => {
+    console.log("error", error)
+})
