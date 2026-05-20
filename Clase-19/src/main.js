748c2a50640222811abfdc808e0985fdf9fd34b3
matu.dev <mati.a.gimenez2002@gmail.com>
2026-05-19 22:03:53 -0300

    Clase 19

--- Clase-19/src/main.js
+++ Clase-19/src/main.js
@@ -56,7 +56,8 @@ Ruta: /api/workspace
 
 
     controlador: workspaceController
-        post() Debe estar con el authMiddleware
+        
+        POST '/' post() Debe estar con el authMiddleware (IMPORTANTE)
             Validar nombre y descripcion (opcional)
             Crear un espacio de trabajo
             Crear una membresia de role tipo 'dueño' a nombre del id del cliente consultante.
@@ -65,8 +66,36 @@ Ruta: /api/workspace
                 nombre,
                 descripcion
             }
-            
 
+        GET '/' getAllByUser() Debe estar con el authMiddleware (IMPORTANTE)
+            Buscar todos los espacios de trabajo de los que el cliente consultante es miembro 
+            Responder con la lista de espacios de trabajo
+
+        DELETE '/:workspace_id' deleteById() Debe estar con el authMiddleware
+            Validar que el espacio de trabajo exista => 404
+            Validar que el usuario consultante sea 'dueño' de dicho espacio de trabajo => 403 Forbidden
+            Eliminar (Soft o Hard) el espacio de trabajo
+
+        PUT '/:workspace_id' updateById() Debe estar con el authMiddleware
+            body: {
+                nombre (opcional),
+                descripcion (opcional)
+            }
+            Validar que el espacio de trabajo exista => 404
+            Validar que el usuario consultante sea 'dueño' o 'admin' de dicho espacio de trabajo => 403 Forbidden
+            Actualizar los campos correspondientes.
+
+    RECOMENDACION:
+        Como se repite 
+            Validar que el espacio de trabajo exista
+            Validar que el cliente consultante sea miembro del espacio de trabajo
+        Vendria muy bien usar un middleware que se llame workspaceMiddleware
+        Haria:
+            - Validar que el espacio de trabajo exista
+            - Validar que el cliente consultante sea miembro del espacio de trabajo
+            - Guardar en la request la info de:
+                workspace
+                member
 */
 
 
